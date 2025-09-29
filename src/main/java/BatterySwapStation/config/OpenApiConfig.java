8b9413afe62fdcb5579960aad08543a95bc98f28
karmalinh <karmalinh0201@gmail.com>
2025-09-29 11:40:00 +0100

    update openapiconfig

--- src/main/java/BatterySwapStation/config/OpenApiConfig.java
+++ src/main/java/BatterySwapStation/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Localhost"),
-                        new Server().url("https://swp-be-demo.up.railway.app").description("Railway")
+                        new Server().url("https://batteryswap-be-production.up.railway.app").description("Railway")
                 ));
     }
 }
