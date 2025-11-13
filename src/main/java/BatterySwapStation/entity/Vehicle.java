8e41de287b00a5369d1b4e3be70bac4417fbb395
karmalinh <karmalinh0201@gmail.com>
2025-11-13 14:55:34 +0000

    chỉnh sưa swap, thêm bảng trung gian giữa pin và xe

--- src/main/java/BatterySwapStation/entity/Vehicle.java
+++ src/main/java/BatterySwapStation/entity/Vehicle.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "Vehicle", indexes = {
         @Index(name = "idx_vehicle_vin", columnList = "VIN", unique = true)
@@ -64,6 +67,10 @@ public class Vehicle {
 
     @Column(name = "ownername", length = 100)
     private String ownerName;
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<VehicleBattery> vehicleBatteries = new ArrayList<>();
+
 
     @Override
     public boolean equals(Object o) {
